bbcca70a3cf9f818fc008edba83ed08dfdacd7b2
Alex Ruffo <Alex Ruffo>
2016-03-12 18:23:21 -0500

    key added

---
+++ Pages/videoPlayer.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Microsoft.Samples.Kinect.ControlsBasics
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ButtonSample
+    /// </summary>
+    public partial class videoplayer : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ButtonSample" /> class.
+        /// </summary>
+        public videoplayer()
+        {
+            this.InitializeComponent();
+            IsPlaying(false);
+        }
+        private void IsPlaying(bool flag)
+        {
+            playbutton.IsEnabled = flag;
+            // btnStop.IsEnabled = flag;
+            // btnMoveBack.IsEnabled = flag;
+            // btnMoveForward.IsEnabled = flag;
+        }
+    }
+}
